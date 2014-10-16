9194566303b8d48c2c84ec54a81ce679cc73df49
evlkls <lue11001@byui.edu>
2014-10-16 17:20:17 -0500

    Is it working?

---
+++ src/connect4/StatsMenuView.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connect4;
+
+import java.util.Scanner;
+/**
+ *
+ * @author SexyMama
+ */
+public class StatsMenuView {
+    private final static String[][] menuItems = {
+        {"1", "Player 1 Statistics"},
+        {"2", "Player 2 Statistics"}, 
+        {"H", "Help Menu"},       
+        {"Q", "Quit Statistics"}        
+    };
+    
+    // Create instance of the HelpMenuControl (action) class
+    private StatsMenuController statsMenuController = new StatsMenuController();
+    
+    // default constructor
+    public StatsMenuView() {
+        
+    } 
+    
+    // display the help menu and get the end users input selection
+    public void getInput() {       
+              
+        String command;
+        Scanner inFile = new Scanner(System.in);
+        
+        do {
+            
+            this.display(); // display the menu
+            
+            // get commaned entered
+            command = inFile.nextLine();
+            command = command.trim().toUpperCase();
+            
+            switch (command) {
+                case "1":
+                    this.statsMenuController.displayOneStats();
+                    break;
+                case "2":
+                    this.statsMenuController.displayTwoStats();
+                    break;
+                case "H":
+                    this.statsMenuController.displayHelp();
+                    break;                  
+                case "Q":
+                    break;
+                default: 
+                    new Connect4Error().displayError("Invalid command. Please enter a valid command.");
+                    continue;
+            }
+        } while (!command.equals("Q"));  
+        
+         return;
+    }
+
+        // displays the help menu
+    public final void display() {
+        System.out.println("\n\t===============================================================");
+        System.out.println("\tEnter the letter associated with one of the following commands:");
+
+        for (int i = 0; i < StatsMenuView.menuItems.length; i++) {
+            System.out.println("\t   " + menuItems[i][0] + "\t" + menuItems[i][1]);
+        }
+        System.out.println("\t===============================================================\n");
+    }
+  
+}
