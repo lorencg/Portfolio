0807fea0ae0c64c62b81287e9ef7db9b28f0a518
jsiqueiros <jsiqueiros@jsiqueiros.kyani.local>
2014-10-23 23:00:16 -0600

    Added to Token class a selector for Red or Black.

--- src/connect4/Tokens.java
+++ src/connect4/Tokens.java
@@ -6,6 +6,9 @@
  */
 package connect4;
 
+import static java.lang.System.in;
+import java.util.Scanner;
+
 /**
  *
  * @author Loren
@@ -13,9 +16,38 @@ package connect4;
 public class Tokens {
     String color = "red or black";
     int size = 5;
-    
-    public void displayToken(){
+    Scanner in = new Scanner(System.in);
+       int x;
+       boolean red = false;
+       boolean black = false; 
+       String input = in.next();
+   
+       public void displayToken(){
         System.out.println(this.color + " with the size of: " + this.size);
     }
-
-}
+     // Author Faron, Josh, Sarah
+    public void tokenColor() {
+        // This function when called will take input and will check if R is input then if it is return true to red, Same to black.
+        for (x=0  ; x<2 ;  x++ ) {
+            if ("R".equals(input) ){
+                red = true;
+                 return;
+            }
+            
+            else if("B".equals(input)) {
+                    black = true;
+                     return;  
+            }
+            else
+                System.out.println("Error...Please choose R for Red or B for Black." );
+                return;
+                
+                
+                                      
+            }
+        
+        }
+           
+           
+           
+       } 
